96e4ca9b276d14b82eed792146c00e40f70cf089
sakejha34 <saketjha0324@gmail.com>
2024-05-04 15:59:17 +0530

    Create Metrics.py

---
+++ Metrics/Metrics.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+
+def root_mean_squared_error(y_true, y_pred):
+    return np.sqrt(np.mean(y_true - y_pred)**2)
+
+def mean_squared_error(y_true, y_pred):
+    return np.mean(y_true - y_pred)**2
+
+def mean_absolute_error(y_true, y_pred):
+    return np.mean(np.abs(y_true - y_pred))
+
+def mean(x):
+    return np.mean(x)
+
+def mean_deviation(x):
+    return np.mean(np.abs(x-np.mean(x)))
+
+def variance(x):
+    return np.mean((x - np.mean(x))**2)
+
+def standard_deviation(x):
+    return np.sqrt(np.mean((x-np.mean(x))**2))
+
+def r_2_score(y_true, y_pred):
+    corr_matrix = np.corrcoef(y_true, y_pred)
+    corr = corr_matrix[0,1]
+    return corr**2
+
+def max_error(y_true , y_pred):
+    return np.max(np.abs(y_true - y_pred))
