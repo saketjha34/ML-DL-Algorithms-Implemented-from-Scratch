0333f1f367242610e836e646175b006d6fff15eb
sakejha34 <saketjha0324@gmail.com>
2024-05-25 09:12:27 +0530

    Update Metrics.py

--- Metrics/Metrics.py
+++ Metrics/Metrics.py
@@ -41,8 +41,5 @@ def accuracy_score(y_true , y_pred):
 def precision_score(y_true , y_pred):
     return 
 
-y_true = np.array([1,0,1,0,1])
-y_pred = np.array([0,0,1,0,1])
-print(accuracy_score(y_pred=y_pred,y_true=y_true))
 
 
